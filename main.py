d7c9e6d6af5cefe9c7dba0fa6177f6efa60d6273
vokin <kinbiceps@mail.ru>
2024-09-11 12:10:57 +0300

    put and patch methods

---
+++ main.py
@@ -0,0 +1,83 @@
+import uvicorn
+from fastapi import FastAPI, Query, Body
+from fastapi.openapi.docs import get_swagger_ui_html
+
+app = FastAPI()
+
+hotels = [
+    {"id": 1, "title": "Sochi", "name": "Radisson"},
+    {"id": 2, "title": "Дубай", "name": "Burj Al Arab"},
+]
+
+
+@app.get("/hotels")
+def get_hotels(
+        id: int | None = Query(None, description="Айдишник"),
+        title: str | None = Query(None, description="Название отеля"),
+):
+    hotels_ = []
+    for hotel in hotels:
+        if id and hotel["id"] != id:
+            continue
+        if title and hotel["title"] != title:
+            continue
+        hotels_.append(hotel)
+    return hotels_
+
+
+@app.post("/hotels", summary="Создание отеля")
+def create_hotel(
+        title: str = Body(embed=True),
+):
+    global hotels
+    hotels.append({
+        "id": hotels[-1]["id"] + 1,
+        "title": title
+    })
+    return {"status": "OK"}
+
+
+@app.delete("/hotels/{hotel_id}", summary="Удаление отеля")
+def delete_hotel(hotel_id: int):
+    global hotels
+    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
+    return {"status": "OK"}
+
+
+@app.put("/hotels/{hotel_id}", summary="Обновление отеля")
+def put_hotel(hotel_id: int, title: str = Body(), name: str = Body()):
+    global hotels
+    for hotel in hotels:
+        if hotel["id"] == hotel_id:
+            hotel["title"] = title
+            hotel["name"] = name
+            break
+    return {"status": "OK"}
+
+
+@app.patch("/hotels/{hotel_id}", summary="Частичное обновление отеля")
+def patch_hotel(hotel_id: int, title: str | None = Body(), name: str | None = Body()):
+    global hotels
+    for hotel in hotels:
+        if hotel["id"] == hotel_id:
+            if title:
+                hotel["title"] = title
+            if name:
+                hotel["name"] = name
+            break
+    return {"status": "OK"}
+
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
+        swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
+    )
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
