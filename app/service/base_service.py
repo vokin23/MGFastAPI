b5285a33138aff0e1337a8a34569ce325ad74809
vokinsilok <kinbiceps@mail.ru>
2024-09-23 11:26:41 +0300

    fix auction app

--- app/service/base_service.py
+++ app/service/base_service.py
@@ -3,5 +3,5 @@ from datetime import datetime
 
 def get_moscow_time():
     moscow_tz = pytz.timezone('Europe/Moscow')
-    moscow_time = datetime.now(moscow_tz)
+    moscow_time = datetime.now(moscow_tz).replace(tzinfo=None)
     return moscow_time
