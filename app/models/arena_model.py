32fde5cb5eac002659feee26d57c8c2bfbc6bd47
vokinsilok <kinbiceps@mail.ru>
2024-09-27 13:19:39 +0300

    start arena app

--- app/models/arena_model.py
+++ app/models/arena_model.py
@@ -11,9 +11,9 @@ class Arena(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(100))
     description: Mapped[str] = mapped_column(String(100))
-    cords: Mapped[JSON] = mapped_column(JSON, nullable=True, default=None)
+    cords_spawn: Mapped[JSON] = mapped_column(JSON, nullable=True, default=None)
     cloths: Mapped[JSON] = mapped_column(JSON, nullable=True, default=None)
-    status: Mapped[bool] = mapped_column(default=True)
+    free: Mapped[bool] = mapped_column(default=True)
 
 
 class Match(Base):
@@ -23,13 +23,13 @@ class Match(Base):
     arena: Mapped[int] = mapped_column(ForeignKey('arena.id'))
     player1: Mapped[int] = mapped_column(ForeignKey('player.id'), nullable=True, default=None)
     old_things_player1: Mapped[JSON] = mapped_column(JSON, nullable=True, default=None)
-    new_things_player1: Mapped[JSON] = mapped_column(JSON, nullable=True, default=None)
+    old_cords_player1: Mapped[JSON] = mapped_column(JSON, nullable=True, default=None)
     player2: Mapped[int] = mapped_column(ForeignKey('player.id'), nullable=True, default=None)
     old_things_player2: Mapped[JSON] = mapped_column(JSON, nullable=True, default=None)
-    new_things_player2: Mapped[JSON] = mapped_column(JSON, nullable=True, default=None)
+    old_cords_player2: Mapped[JSON] = mapped_column(JSON, nullable=True, default=None)
     time_created: Mapped[datetime] = mapped_column(DateTime, default=datetime.now())
     time_start: Mapped[datetime] = mapped_column(DateTime, default=datetime.now())
     time_end: Mapped[datetime] = mapped_column(DateTime, default=datetime.now())
-    full: Mapped[bool] = mapped_column(default=False)
+    start: Mapped[bool] = mapped_column(default=False)
     finished: Mapped[bool] = mapped_column(default=False)
     winner: Mapped[int] = mapped_column(ForeignKey('player.id'), nullable=True, default=None)
