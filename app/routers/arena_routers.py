0cd3e6ba7f71ee8ac15f5909683d2b1360a7c67c
vokin23 <kinbiceps@mail.ru>
2024-10-01 23:13:23 +0300

    Мейн на данный момент

--- app/routers/arena_routers.py
+++ app/routers/arena_routers.py
@@ -6,7 +6,8 @@ from app.models.arena_model import Arena, Match
 from app.models.datebase import async_session_maker
 from app.models.player_model import Player
 from app.schemas.arena_schemas import ArenaCreateSchema, ArenaBaseSchema, MatchReturnSchema, MSGArenaSchema
-from app.service.base_service import get_moscow_time
+from app.service.arena_service import ArenaService
+
 
 arena_queue = []
 
@@ -92,47 +93,29 @@ async def register_arena(data: dict) -> List[Union[MatchReturnSchema, MSGArenaSc
         if player_match and player_match.id in arena_queue:
             return [MSGArenaSchema(steam_id=player.steam_id, msg="Вы уже зарегистрированы на арену")]
 
-        # Получаем все матчи, где нет одного из игроков
-        free_matches_objs = select(Match).where(Match.start == False, Match.finished == False,
-                                                Match.player1 == None or Match.player2 == None)
-        free_matches = await session.execute(free_matches_objs)
-        free_matches = free_matches.scalars().all()
-
-        if free_matches:
-            match = free_matches[0]
-            if not match.player1:
-                await session.execute(update(Match).where(Match.id == match.id).values(player1=player.id, old_things_player1=player.cloths))
-            else:
-                await session.execute(update(Match).where(Match.id == match.id).values(player2=player.id, old_things_player2=player.cloths))
-            await session.commit()
-        else:
-            new_match_obj = insert(Match).values(player1=player.id,
-                                                 old_things_player1=player.cloths,
-                                                 time_created=get_moscow_time()
-                                                 ).returning(Match)
-            new_match_stmt = await session.execute(new_match_obj)
-            new_match = new_match_stmt.scalar()
-            await session.commit()
-            arena_queue.append(new_match.id)
-
-        free_arenas_objs = select(Arena).where(Arena.free == True)
-        free_arenas = await session.execute(free_arenas_objs)
-        free_arenas = free_arenas.scalars().all()
-
-        new_start_matches = []
-        if free_arenas:
-            for free_arena in free_arenas:
-                match = arena_queue.pop(0)
-                await session.execute(update(Match).where(Match.id == match).values(arena=free_arena.id, start=True, time_start=get_moscow_time()))
-                await session.commit()
-                new_start_matches.append(MatchReturnSchema(cords_spawn=free_arena.cords_spawn,
-                                                           player1=match.player1,
-                                                           player2=match.player2,
-                                                           cloths_player1=match.old_things_player1,
-                                                           cloths_player2=match.old_things_player2))
-        return new_start_matches if new_start_matches else [None]
+        await ArenaService.check_free_matches_or_create_new(session, arena_queue, player)
+        await ArenaService.if_free_arena_start_matches(session, arena_queue)
 
 
+@arena_router.post("/delete_register_arena", summary="Удаление регистрации на Арену")
+async def delete_register_arena(data: dict) -> MSGArenaSchema:
+    async with async_session_maker() as session:
+        player_obj = select(Player).where(Player.steam_id == data.get('steam_id'))
+        player_stmt = await session.execute(player_obj)
+        player = player_stmt.scalar()
+
+        if not player:
+            raise HTTPException(status_code=404, detail="Игрок не найден")
+
+        player_match_obj = select(Match).where(or_(Match.player1 == player.id, Match.player2 == player.id), Match.finished == False)
+        player_match_stmt = await session.execute(player_match_obj)
+        player_match = player_match_stmt.scalar()
+        if player_match and player_match.id in arena_queue:
+            arena_queue.remove(player_match.id)
+            return MSGArenaSchema(steam_id=player.steam_id, msg="Вы успешно удалились с арены")
+
+
+#TODO: Доработать метод обновления матча и создать вывод информации для КПК
 @arena_router.post("/update_arena_match", summary="Обновить\завершить матч")
 async def update_arena_match(data: dict) -> List[MatchReturnSchema]:
     async with async_session_maker() as session:
